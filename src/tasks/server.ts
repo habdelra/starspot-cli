72593946b05d19d0c85b00f334072f1b69287c0e
Tom Dale <tom@tomdale.net>
2016-08-22 17:03:27 -0400

    Fall back to http if certs aren’t available

--- src/tasks/server.ts
+++ src/tasks/server.ts
@@ -1,9 +1,10 @@
 import * as dotenv from "dotenv";
 import { Application } from "starspot";
+import * as http from "http";
 import * as http2 from "http2";
 import { readFile } from "mz/fs";
 import Task from "../task";
-import SSLNotFoundError from "../errors/ssl-not-found-error";
+// import SSLNotFoundError from "../errors/ssl-not-found-error";
 import { SSL_KEY_PATH, SSL_CERT_PATH, DNS_TLD } from "../config";
 
 export interface ServerAddressInfo {
@@ -25,9 +26,26 @@ export default class ServerTask extends Task<ServerAddressInfo> {
     let [key, cert] = await readSSLCerts();
 
     return new Promise<ServerAddressInfo>((resolve, reject) => {
-      let server = http2.createServer({ key, cert }, (request, response) => {
-        app.dispatch(request, response);
-      });
+      interface Server {
+        on(eventName: string, callback: any): void;
+        address(): ServerAddressInfo;
+        listen(port: number): void;
+      };
+
+      let server: Server;
+      let protocol: string;
+
+      if (key && cert) {
+        server = http2.createServer({ key, cert }, (request, response) => {
+          app.dispatch(request, response) as any as Server;
+        });
+        protocol = "https";
+      } else {
+        server = http.createServer((request, response) => {
+          app.dispatch(request as any as Application.Request, response);
+        }) as any as Server;
+        protocol = "http";
+      }
 
       server.on("error", reject);
 
@@ -38,7 +56,7 @@ export default class ServerTask extends Task<ServerAddressInfo> {
           address: info.address,
           port: info.port,
           family: info.family,
-          url: urlForAddressInfo(info)
+          url: urlForAddressInfo(info, protocol)
         });
       });
 
@@ -61,18 +79,19 @@ async function readSSLCerts(): Promise<[Buffer, Buffer]> {
       readFile(SSL_CERT_PATH)
     ]);
   } catch (e) {
-    if (e.code === "ENOENT") {
-      throw new SSLNotFoundError({
-        missingFile: e.path
-      });
-    }
+    return [null, null];
+    // if (e.code === "ENOENT") {
+    //   throw new SSLNotFoundError({
+    //     missingFile: e.path
+    //   });
+    // }
   }
 }
 
-function urlForAddressInfo({ address, family, port }: ServerAddressInfo): string {
+function urlForAddressInfo({ address, family, port }: ServerAddressInfo, protocol: string): string {
   if (family === "IPv6") {
     address = `[${address}]`;
   }
 
-  return `https://${process.env.USER}.${DNS_TLD}:${port}`;
+  return `${protocol}://${process.env.USER}.${DNS_TLD}:${port}`;
 }
