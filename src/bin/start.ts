7e52e47df3d559f6ab45fdd2348ff7a7767989f5
Tom Dale <tom@tomdale.net>
2016-08-23 16:57:45 -0400

    Require Node 6 or later

--- src/bin/start.ts
+++ src/bin/start.ts
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
 
 import "ts-node/register";
-
 import { red } from "chalk";
+
+let version = Number(process.version.match(/^v(\d+\.\d+)/)[1])
+if (version < 6) {
+  console.log(red(`Starspot requires Node 6 or later. You are currently using Node ${process.version}`));
+  process.exit(1);
+}
+
 import { fork } from "mz/child_process";
 import Project from "../project";
 import SetupTask from "../tasks/setup";
